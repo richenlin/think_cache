a3dca89bba1c652c086b555f1296fe75bd30f673
yanglin <richenlin@gmail.com>
2017-06-12 09:59:12 +0800

    .gitignore

--- src/base.js
+++ src/base.js
@@ -10,7 +10,7 @@ const lib = require('think_lib');
 module.exports = class {
     constructor(options = {}) {
         this.options = lib.extend({
-            cache_type: 'File', //数据缓存类型 File,Redis,Memcache
+            cache_type: 'file', //数据缓存类型 file,redis,memcache
             cache_key_prefix: 'ThinkKoa:', //缓存key前置
             cache_timeout: 6 * 3600, //数据缓存有效期，单位: 秒
             cache_file_suffix: '.json', //File缓存方式下文件后缀名
