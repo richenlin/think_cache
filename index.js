d8e4f1bbaa78ec0472dda78b76499fe89b2484e7
yanglin <richenlin@gmail.com>
2017-07-05 11:12:36 +0800

    将store独立成为模块think_store

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  * @version    17/6/6
  */
 const lib = require('think_lib');
+const store = require('think_store');
 /**
  * default options
  */
@@ -13,27 +14,32 @@ const defaultOptions = {
     cache_type: 'file', //数据缓存类型 file,redis,memcache
     cache_key_prefix: 'ThinkKoa:', //缓存key前置
     cache_timeout: 6 * 3600, //数据缓存有效期，单位: 秒
-    cache_file_suffix: '.json', //File缓存方式下文件后缀名
-    cache_gc_hour: [4], //缓存清除的时间点，数据为小时
-    cache_path: think.root_path + '/cache', //文件类缓存目录
 
-    memcache_host: '127.0.0.1',
-    memcache_port: 11211,
-    memcache_poolsize: 10, //memcache pool size
-    memcache_timeout: 10, //try connection timeout, 
+    //cache_type=file
+    file_suffix: '.json', //File缓存方式下文件后缀名
+    file_path: think.root_path + '/cache',
 
+    //cache_type=redis
     redis_host: '127.0.0.1',
     redis_port: 6379,
     redis_password: '',
     redis_db: '0',
     redis_timeout: 10, //try connection timeout
+
+    //cache_type=memcache
+    memcache_host: '127.0.0.1',
+    memcache_port: 11211,
+    memcache_poolsize: 10, //memcache pool size
+    memcache_timeout: 10, //try connection timeout,
 };
 
 module.exports = function (options) {
     options = options ? lib.extend(defaultOptions, options, true) : defaultOptions;
     think.app.once('appReady', () => {
-        options.cache_key_prefix = (~((options.cache_key_prefix).indexOf(':'))) ? `${options.cache_key_prefix}Cache:` : `${options.cache_key_prefix}:Cache:`;
-        think._caches._stores = require(`./lib/adapter/${options.cache_type || 'file'}.js`);
+        options.type = options.cache_type || 'file'; //数据缓存类型 file,redis,memcache
+        options.key_prefix = (~((options.cache_key_prefix).indexOf(':'))) ? `${options.cache_key_prefix}Cache:` : `${options.cache_key_prefix}:Cache:`; //缓存key前缀
+        options.timeout = options.cache_timeout || 6 * 3600; //数据缓存有效期，单位: 秒
+        think._caches._cache = store.getInstance(options);
 
         lib.define(think, 'cache', function (name, value, option) {
             try {
@@ -42,16 +48,14 @@ module.exports = function (options) {
                 } else if (option === null) {
                     options.cache_timeout = null;
                 }
-
-                let instance = new (think._caches._stores)(options);
                 if (value === undefined) {
-                    return instance.get(name).then(val => {
+                    return think._caches._cache.get(name).then(val => {
                         return lib.isJSONStr(val) ? JSON.parse(val) : val;
                     });
                 } else if (value === null) {
-                    return instance.rm(name);
+                    return think._caches._cache.rm(name);
                 } else {
-                    return instance.set(name, (lib.isBoolean(value) || lib.isNumber(value) || lib.isString(value)) ? value : JSON.stringify(value), options.cache_timeout);
+                    return think._caches._cache.set(name, (lib.isBoolean(value) || lib.isNumber(value) || lib.isString(value)) ? value : JSON.stringify(value), options.cache_timeout);
                 }
             } catch (e) {
                 return null;
