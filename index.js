62c0706b4126dde3c6b4e46f84bd422fb8df28d6
richenlin <richenlin@gmail.com>
2020-03-05 15:23:13 +0800

    Update

--- index.js
+++ index.js
@@ -11,61 +11,55 @@ const store = require('think_store');
  * default options
  */
 const defaultOptions = {
-    cache_type: 'file', //数据缓存类型 file,redis,memcache
-    cache_key_prefix: 'ThinkKoa:', //缓存key前置
-    cache_timeout: 6 * 3600, //数据缓存有效期，单位: 秒
+    type: 'file', //数据缓存类型 file,redis,memcache
+    key_prefix: 'ThinkKoa:', //缓存key前置
+    timeout: 6 * 3600, //数据缓存有效期，单位: 秒
 
-    //cache_type=file
+    //type=file
     file_suffix: '.json', //File缓存方式下文件后缀名
+    gc_hour: [4], //缓存清除的时间点，数据为小时
     file_path: process.env.ROOT_PATH + '/cache',
 
-    //cache_type=redis
-    redis_host: '127.0.0.1',
-    redis_port: 6379,
-    redis_password: '',
-    redis_db: '0',
-    redis_timeout: 10, //try connection timeout
+    //type=redis
+    // host: '127.0.0.1',
+    // port: 6379,
+    // password: '',
+    // db: '0',
 
-    //cache_type=memcache
-    memcache_host: '127.0.0.1',
-    memcache_port: 11211,
-    memcache_poolsize: 10, //memcache pool size
-    memcache_timeout: 10, //try connection timeout,
+    //type=memcache
+    // host: '127.0.0.1',
+    // port: 11211,
+
+    poolsize: 10, //pool size
+    conn_timeout: 5000, //try connection timeout, 
 };
 
 module.exports = function (options, app) {
     options = options ? lib.extend(defaultOptions, options, true) : defaultOptions;
-    app.once('appReady', () => {
-        options.type = options.cache_type || 'file'; //数据缓存类型 file,redis,memcache
-        options.key_prefix = (~((options.cache_key_prefix).indexOf(':'))) ? `${options.cache_key_prefix}Cache:` : `${options.cache_key_prefix}:Cache:`; //缓存key前缀
-        options.timeout = options.cache_timeout || 6 * 3600; //数据缓存有效期，单位: 秒
-        
-        app._caches.store = store.getInstance(options);
+    options.type = options.type || 'file'; //数据缓存类型 file,redis,memcache
+    options.key_prefix = (~((options.key_prefix).indexOf(':'))) ? `${options.key_prefix}Cache:` : `${options.key_prefix}:Cache:`; //缓存key前缀
+    options.timeout = options.timeout || 6 * 3600; //数据缓存有效期，单位: 秒
 
-        lib.define(app, 'cache', function (name, value, option) {
-            try {
-                if (lib.isNumber(option)) {
-                    options.cache_timeout = option;
-                } else if (option === null) {
-                    options.cache_timeout = null;
-                }
-                if (value === undefined) {
-                    return app._caches.store.get(name).then(val => {
-                        return lib.isJSONStr(val) ? JSON.parse(val) : val;
-                    });
-                } else if (value === null) {
-                    return app._caches.store.rm(name);
-                } else {
-                    return app._caches.store.set(name, (lib.isBoolean(value) || lib.isNumber(value) || lib.isString(value)) ? value : JSON.stringify(value), options.cache_timeout);
-                }
-            } catch (e) {
-                return null;
-            }
-        });
-        return;
+    const storeConn = async function (opt) {
+        const cacheStore = new store(opt);
+        lib.define(app, 'cacheStore', cacheStore, true);
+
+        if (opt.type !== 'file') {
+            const cacheClient = await cacheStore.connect();
+            lib.define(app, 'cacheClient', cacheClient, true);
+        }
+    };
+
+    //应用启动执行一次
+    app.once('appReady', async () => {
+        await storeConn(options);
     });
 
-    return function (ctx, next) {
+    return async function (ctx, next) {
+        if (!app.cacheStore || (app.cacheStore.connect && (!app.cacheClient || app.cacheClient.status !== 'ready'))) {
+            await storeConn(options);
+        }
+
         return next();
     };
 };
